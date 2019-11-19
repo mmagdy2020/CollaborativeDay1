dd98b270c49a9709be9d8af31b4af106ebcafcd4
Mahmoud131 <ma7moud_magdy@yahoo.com>
2019-11-18 22:58:44 -0600

    create a new function

--- code.js
+++ code.js
@@ -1,1 +1,12 @@
-alert("Hi")
+//alert("Hi")
+
+createAnewFunction([10, 20, 30])
+
+function createAnewFunction(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+        let printed = arr[i]
+        console.log(printed)
+    }
+
+}
